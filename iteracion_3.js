a910abf970f4d118c732d63d6418bd1b2081faad
unknown <giselaclaure@gmail.com>
2021-10-05 15:03:13 +0200

    iteracion_3

---
+++ iteracion_3.js
@@ -0,0 +1,23 @@
+/* 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.log. */
+let numeroA=  10
+let numeroB= 5
+console.log(numeroA*numeroB);
+
+/* 1.2 Divide 10 por 2 y muestra el resultado en un console.log. */
+let numeroC= 2
+console.log(numeroA/numeroC);
+
+/* 1.3 Muestra mediante un console.log el resto de dividir 15 por 9. */
+let numeroD= 15
+let numeroE= 9
+console.log(numeroD/numeroE);
+
+/* 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+teniendo dos variables y = 10 y z = 5. */
+let y= 10
+let z= 5
+console.log (y+z)
+
+/* 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+teniendo dos variables y = 10 y z = 5. */
+console.log (y*z)
